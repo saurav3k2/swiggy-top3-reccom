ab8925d09f2dd1efb2db3d005c78152c49a50f48
Saurav Kumar <saurav3k2@gmail.com>
2026-08-26 15:11:27 +0530

    files added due to removed of data folder before  dvc tracking

--- swiggy_top3_reccom/dataset.py
+++ swiggy_top3_reccom/dataset.py
@@ -1,29 +1,8 @@
-from pathlib import Path
+import pandas as pd
 
-from loguru import logger
-from tqdm import tqdm
-import typer
-
-from swiggy_top3_reccom.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
-
-app = typer.Typer()
+# 1. reading data from csv file
+def read_csv(file_data):
+    return pd.read_csv(file_data)
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
-
-
-if __name__ == "__main__":
-    app()
+    
